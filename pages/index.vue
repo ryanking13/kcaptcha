188b6807b775a7657e4ab22d23347b5eeb27fa95
ryanking13 <def6488@gmail.com>
2020-09-04 18:00:52 +0900

    Implement mobilenet prediction logic

--- pages/index.vue
+++ pages/index.vue
@@ -4,13 +4,12 @@
       <no-ssr>
         <paste-handler :onPaste="updateCaptchaImage" />
       </no-ssr>
-      <dropzone :onAdd="check" />
+      <dropzone :onAdd="loadCaptchaImage" />
       <img
-        v-if="captchaSrc"
+        v-show="captchaSrc"
         id="captcha"
         :src="captchaSrc"
         alt="Uploaded CAPTCHA Image"
-        style="width: 160px; height: 60px"
       />
     </div>
   </section>
@@ -30,13 +29,20 @@
 import Card from "~/components/Card";
 import Dropzone from "~/components/Dropzone";
 import PasteHandler from "~/components/PasteHandler";
+import * as tf from "@tensorflow/tfjs";
 
 export default {
   name: "HomePage",
+  async created() {
+    this.model = await tf.loadGraphModel("http://localhost:3000", {
+      fromTFHub: true,
+    });
+  },
 
   data() {
     return {
       captchaSrc: "",
+      model: null,
     };
   },
 
@@ -49,14 +55,34 @@ export default {
   methods: {
     updateCaptchaImage: function (src) {
       this.captchaSrc = src;
+      setTimeout(() => {
+        this.predictCaptcha();
+      }, 10); // prevent pre predicting
     },
-    check: function (file) {
+    loadCaptchaImage: function (file) {
       var reader = new FileReader();
       reader.onload = (event) => {
         // console.log(event.target.result); // data url!
         this.updateCaptchaImage(event.target.result);
       };
       reader.readAsDataURL(file);
+    },
+    predictCaptcha: function () {
+
+      // Get HTMLImageElement from the document
+      const imgElem = document.getElementById("captcha");
+      // Convert HTMLImageElement to pixel data
+      const img = tf.browser.fromPixels(imgElem).toFloat().resizeNearestNeighbor([224, 224])
+      // img.print()
+
+      // Normalize image to fit Model input format
+      const imgNormalized = img.div(tf.scalar(255));
+      const imgNormalizedBatch = imgNormalized.expandDims(0);
+
+      // console.log(imgNormalizedBatch)
+      const result = this.model.predict(imgNormalizedBatch);
+
+      tf.argMax(result, 1).print()
     },
   },
 };
